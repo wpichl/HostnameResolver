c3afabf2341d86e096083e8967efb6ed7a2131f4
wpichl <walter.pichl@hotmail.com>
2021-01-21 16:02:57 +0100

    feat: add basic program

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include<stdio.h> //printf
+#include<string.h> //memset
+#include<stdlib.h> //for exit(0);
+
+#include "resolve.h"
+
+int main(int argc, char* argv[])
+{
+	if(argc < 2)
+	{
+		printf("Please provide a hostname to resolve");
+		exit(1);
+	}
+	
+	char* hostname = argv[1];
+	char ip[100];
+
+	resolve(hostname , ip);
+	printf("%s resolved to %s" , hostname , ip);
+	printf("\n");
+}
